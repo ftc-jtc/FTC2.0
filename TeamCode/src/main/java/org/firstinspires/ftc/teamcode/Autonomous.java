30fec0bd3bcb94b8ae557d769cf82ddea5c10670
JTC-main <first.jtc@gmail.com>
2019-02-07 18:53:59 +0100

    Final code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -0,0 +1,93 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by standalone on 14-5-2018.
+ */
+
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Autonomous_Yeeeee", group="Auto")
+
+public class Autonomous extends LinearOpMode {
+
+    DcMotor LeftDrive = null;
+    DcMotor RightDrive = null;
+    Servo Klep = null;
+    DcMotor LiftSysteem = null;
+    DcMotor LiftSysteem2 = null;
+    DcMotor LiftSysteem3 = null;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        LeftDrive = hardwareMap.dcMotor.get("LeftDrive");
+        RightDrive = hardwareMap.dcMotor.get("RightDrive");
+        Klep = hardwareMap.servo.get("Klep");
+        LiftSysteem = hardwareMap.dcMotor.get("LiftSysteem");
+        LiftSysteem2 = hardwareMap.dcMotor.get("LiftSysteem2");
+        LiftSysteem3 = hardwareMap.dcMotor.get("Liftsysteem3");
+
+        waitForStart();
+
+        DriveForwardTime(1, 900);
+        DriveForwardTime(0, 100);
+        LiftTime(0.2, 1500);
+        LiftTime(-0.2, 1500);
+        TurnRightTime(0.35, 2000);
+        DriveForwardTime(1, 2200);
+        DriveForwardTime(0, 100);
+        LiftTime(0.3, 3000);
+
+
+    }
+
+    public void DriveForward(double power) {
+        LeftDrive.setPower(-power);
+        RightDrive.setPower(power);
+
+    }
+
+    public void DriveForwardTime(double power, long time) throws InterruptedException {
+        DriveForward(power);
+        Thread.sleep(time);
+    }
+
+
+    public void TurnRight(double power) {
+        LeftDrive.setPower(-power);
+        RightDrive.setPower(-power);
+
+    }
+
+    public void TurnRightTime(double power, long time) throws InterruptedException {
+        TurnRight(power);
+        Thread.sleep(time);
+    }
+
+    public void Arm_yeet(double power) {
+        LiftSysteem2.setPower(power);
+    }
+
+    public void Arm_yeettime(double power, long time) throws InterruptedException {
+        Arm_yeet(power);
+        Thread.sleep(time);
+    }
+
+
+    public void Lift3(double power) {
+        LiftSysteem3.setPower(power);
+    }
+
+    public void LiftTime(double power, long time) throws InterruptedException {
+        Lift3(power);
+        Thread.sleep(time);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpmode.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Range;
+
+@TeleOp(name="main: REV", group="all")
+
+public class TeleOpmode extends LinearOpMode
+{
+    private DcMotor LeftDrive;
+    private DcMotor RightDrive;
+    private DcMotor LiftSysteem;
+    private DcMotor LiftSysteem2;
+    private DcMotor LiftSysteem3;
+    private Servo Klep;
+
+    float fl_power;
+    float fr_power;
+    float lift_power;
+    float lift2_power;
+    float lift3_power;
+
+
+    @Override
+    public void runOpMode () throws InterruptedException
+    {
+        LeftDrive = hardwareMap.dcMotor.get("LeftDrive");
+        RightDrive = hardwareMap.dcMotor.get("RightDrive");
+        LiftSysteem = hardwareMap.dcMotor.get("LiftSysteem");
+        LiftSysteem2 = hardwareMap.dcMotor.get("LiftSysteem2");
+        LiftSysteem3 = hardwareMap.dcMotor.get("Liftsysteem3");
+        Klep = hardwareMap.servo.get("Klep");
+
+        waitForStart();
+
+
+        while (opModeIsActive())
+        {
+            fl_power = gamepad1.left_stick_y + gamepad1.right_trigger - gamepad1.left_trigger - gamepad1.right_stick_y;
+            fr_power = -gamepad1.right_stick_y - gamepad1.right_trigger + gamepad1.left_trigger + gamepad1.left_stick_y ;
+            lift_power = -gamepad2.right_stick_y ;
+            lift2_power = gamepad2.right_stick_x;
+            lift3_power = gamepad2.left_stick_y;
+
+            LeftDrive.setPower(fl_power);
+            RightDrive.setPower(fr_power);
+            LiftSysteem.setPower(Range.clip(lift_power, -0.8, 0.8 ));
+            LiftSysteem2.setPower(Range.clip(lift2_power, -0.8, 0.8 ));
+            LiftSysteem3.setPower(Range.clip(lift3_power, -0.3, 0.3));
+
+            if (gamepad2.dpad_up) {
+                Klep.setPosition(0.5);
+            }
+
+            if (gamepad2.dpad_down){
+                Klep.setPosition(0);
+
+            }
+
+            if (gamepad2.dpad_left){
+                Klep.setPosition(0);
+            }
+
+            if (gamepad2.dpad_right){
+                Klep.setPosition(0.5);
+            }
+
+            telemetry.addLine("left side | ")
+                    .addData("trigger", gamepad1.left_trigger)
+                    .addData("y", -gamepad1.left_stick_y);
+            telemetry.addLine("right side | ")
+                    .addData("trigger", gamepad1.right_trigger)
+                    .addData("y", gamepad1.right_stick_y);
+            telemetry.update();
+
+            idle();
+        }
+
+    }
+}
+
